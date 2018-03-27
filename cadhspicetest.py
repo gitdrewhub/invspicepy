849e56d7d7a62b9e51910931ec149adb7afab1c7
desktopdrew <aluc2@asu.edu>
2018-03-26 23:34:34 -0700

    trying to get the fan while loop to work...

--- cadhspicetest.py
+++ cadhspicetest.py
@@ -1,4 +1,3 @@
-
 ###############################
 import subprocess
 import numpy as np
@@ -6,130 +5,146 @@ import shutil
 import re
 import math
 
-tphl_prev=0
-tphl_next=0
-
-
-
-
-
+tphl_prev = 0
+tphl_next = 0
 
 
 def getTPprev():
-    p=subprocess.Popen(["hspice","InvChain1.sp"], stdout=subprocess.PIPE)
-    output,err = p.communicate()
+    p = subprocess.Popen(["hspice", "InvChain1.sp"], stdout=subprocess.PIPE)
+    output, err = p.communicate()
     print(" *** Running hspice InvChain1.sp command ***\n", output)
-    
-    Data = np.recfromcsv("InvChain1.mt0.csv",comments="$",skip_header=3)
+
+    Data = np.recfromcsv("InvChain1.mt0.csv", comments="$", skip_header=3)
     print(Data["tphl_inv"])
-    tphl_prev = Data["tphl_inv"]    
+    tphl_prev = Data["tphl_inv"]
     return tphl_prev
 
 
 def getTPnext():
-    p=subprocess.Popen(["hspice","InvChain1.sp"], stdout=subprocess.PIPE)
-    output,err = p.communicate()
+    p = subprocess.Popen(["hspice", "InvChain1.sp"], stdout=subprocess.PIPE)
+    output, err = p.communicate()
     print(" *** Running hspice InvChain1.sp command ***\n", output)
 
-    Data = np.recfromcsv("InvChain1.mt0.csv",comments="$",skip_header=3)
+    Data = np.recfromcsv("InvChain1.mt0.csv", comments="$", skip_header=3)
     print(Data["tphl_inv"])
     tphl_next = Data["tphl_inv"]
     return tphl_next
 
 
-
-def varyfan():
+def varyfan(invFile1, invFile2):
+    f = open(invFile1, 'r')
+    f1 = open(invFile2, 'w')
     f = open('InvChain1.sp', 'r')
-    f1 = open('InvChain2.sp','w')
-    fannumber=0
+    f1 = open('InvChain2.sp', 'w')
+    fannumber = 0
     for line in f:
-        match = re.search(r'(param\sfan\s=\s)(\d)',line)
-        if match: 
+        match = re.search(r'(param\sfan\s=\s)(\d+)', line)
+        if match:
             numbertxt = match.group(2)
-            fannumber=int(numbertxt)
+            fannumber = int(numbertxt)
             newnumber = int(numbertxt) + 1
             newnumbertxt = str(newnumber)
-            line = re.sub(r'\d',newnumbertxt,line)
+            line = re.sub(r'\d+', newnumbertxt, line)
             f1.write(line)
         else:
             f1.write(line)
-    
+
     f.close()
     f1.close()
-    
-    shutil.copyfile('InvChain2.sp', 'InvChain1.sp') 
+
     return fannumber
 
 
-
-def varyN(beta, index):
-    f = open('InvChain1.sp', 'r')
-    f1 = open('InvChain2.sp','w')
-    index=int(index)
-    beta=int(beta)
+def varyN(beta, index, invFile1, invFile2):
+    f = open(invFile1, 'r')
+    f1 = open(invFile2, 'w')
+    index = int(index)
+    beta = int(beta)
 
     for line in f:
-        match = re.search(r'^X',line)
-        if match: 
+        match = re.search(r'^X', line)
+        if match:
             f1.write('')
         else:
             f1.write(line)
         match = re.search(r'Cload', line)
         if match:
-            line = 'Xinv1 a 2 inv M='+str(beta)+'\n'
+            line = 'Xinv1 a 2 inv M=' + str(
+                    int(beta ** (0))) + '\n'
             f1.write(line)
-            for j in np.arange(1,index-1,1):
-                line = 'Xinv'+str(int(j+1))+' '+str(int(j+1))+' '+str(int(j+2))+' inv M=pow('+str(beta)+','+str(int(j+1))+')\n'
+            for j in np.arange(1, index - 1, 1):
+                line = 'Xinv' + str(int(j + 1)) + ' ' + str(int(j + 1)) + ' ' + str(int(j + 2)) + ' inv M=' + str(
+                    int(beta ** (j))) + '\n'
                 f1.write(line)
-            
-            line = 'Xinv'+str(index+1)+' '+str(index+1)+' z inv M=pow('+str(beta)+','+str(index+1)+')\n'
+
+            line = 'Xinv' + str(index) + ' ' + str(index) + ' z inv M=' + str(int(beta ** (index-1))) + '\n'
             f1.write(line)
     f.close()
     f1.close()
 
-    shutil.copyfile('InvChain2.sp', 'InvChain1.sp') 
 
+R = 50
+shutil.copyfile('InvChain.sp', 'InvChain1.sp')
+shutil.copyfile('InvChain1.sp', 'InvChain2.sp')
+shutil.copyfile('InvChain1.sp', 'InvChain3.sp')
 
+#tpPrev = getTPprev()
+#tpOriginal = tpPrev
 
-
-
-
-
-
-R=50
-tpPrev=getTPprev()
-tpOriginal=tpPrev
 #########First Iteration
-varyfan()
+#varyfan('InvChain1.sp', 'InvChain2.sp')
+#shutil.copyfile('InvChain2.sp', 'InvChain1.sp')
 ####################
-tpNext=getTPnext()
+#tpNext = getTPnext()
+
+
+
+
+'''
+
+
+
 
 
 
 ##while loop
 while tpNext < tpPrev:
-    R=varyfan()
+    R = varyfan('InvChain1.sp', 'InvChain2.sp')
+    shutil.copyfile('InvChain2.sp', 'InvChain1.sp')
     tpPrev = tpNext
-    tpNext= getTPnext()
+    tpNext = getTPnext()
 
+N = math.floor(np.log(R))
+#N = N + (N % 2)
+fan = math.floor(R ** (1 / N))
+varyN(fan, N, 'InvChain3.sp', 'InvChain1.sp')
+tpNext = getTPnext()
+tpPrev = tpOriginal
+'''
+N=1
+fan=1
 
+tpNextN=0.999
+tpPrevN=1
+tpNextF=0.999
+tpPrevF=1
+f=open('comments.txt','w')
+while tpNextF < tpPrevF:
+    N=1
+    tpPrevF=tpNextF
+    while tpNextN < tpPrevN:
+        tpPrevN = tpNextN
 
-N=math.floor(np.log(R))
-fan=math.floor(R**(1/N))
-varyN(fan,N)
-tpNext=getTPnext()
-tpPrev=tpOriginal
+        varyN(fan, N+2, 'InvChain1.sp', 'InvChain2.sp')
+        N += 2
 
-while tpNext < tpPrev:
-    varyN(fan,N)
-    N+=2
-    tpPrev = tpNext
-    tpNext= getTPnext()
-    
-    
+        shutil.copyfile('InvChain2.sp', 'InvChain1.sp')
 
+        tpNextN = getTPnext()
+        if N>15:
+            break
 
-
-
-
-
+    toNextF = tpNextN
+    fan+=1
+    line = 'toPrevF = ' + str(tpPrevF) + ' toNextF = ' + str(toNextF) + '\n'
+    f.write(line)
